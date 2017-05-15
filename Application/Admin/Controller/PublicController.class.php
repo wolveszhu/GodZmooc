33bc6d5029d66b8ddc6142b304ca77fe5a9ab249
bravezhu <13203783812@163.com>
2017-05-15 14:09:12 +0800

    第二次提交

--- Application/Admin/Controller/PublicController.class.php
+++ Application/Admin/Controller/PublicController.class.php
@@ -3,28 +3,23 @@
 	use Think\Controller;
 	
 	class PublicController extends Controller{
-		   
-		   function _initialize(){
+        function _initialize(){
 			
 			 if(!$_SESSION['islogin']){
 				 $this -> error('请先登录',U('Login/login'));
-			  }
+             }
 			  
-			  $uid = $_SESSION['admin']['id'];
-			  if($uid == 1){
-				 
-				  return true;
-				}
+            $uid = $_SESSION['admin']['id'];
+            if($uid == 1){
+                return true;
+            }
 				
 				//后台权限判断
-				 $rule = CONTROLLER_NAME .'/'.ACTION_NAME;
-				
-				 $Auth = new \Think\Auth();
-				 if(!$Auth -> check($rule,$uid)){
-					 $this -> error("你没有权限");
-				 }
-		   }
-		
-		
+            $rule = CONTROLLER_NAME .'/'.ACTION_NAME;
+            $Auth = new \Think\Auth();
+            if(!$Auth -> check($rule,$uid)){
+                $this -> error("你没有权限");
+            }
+        }
 	}
 ?>
